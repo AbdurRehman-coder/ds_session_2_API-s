5afb2cb6c3cfaafe792bf43cbcf7ba48020821d2
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2021-06-23 17:49:50 +0500

    add countries List file

--- lib/screen/Covid_State.dart
+++ lib/screen/Covid_State.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:ds_session_2/RecordComponents/recordComponents.dart';
 import 'package:ds_session_2/model/state_model.dart';
+import 'package:ds_session_2/screen/countriesList.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -30,7 +31,7 @@ class _WorldCovidCasesState extends State<WorldCovidCases> {
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
               'World State',
@@ -45,42 +46,73 @@ class _WorldCovidCasesState extends State<WorldCovidCases> {
             FutureBuilder<Record_Model?>(
               future: getRecordModel(),
               builder: (context, snapshot) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          componentRecord(
-                            title: 'cases',
-                            value: snapshot.data!.cases.toString(),
-                          ),
-                          componentRecord(
-                              title: 'Active cases',
-                              value: snapshot.data!.active.toString()),
-                          componentRecord(
-                              title: 'death',
-                              value: snapshot.data!.deaths.toString()),
-                          componentRecord(
-                              title: 'today cases',
-                              value: snapshot.data!.todayCases.toString()),
-                          componentRecord(
-                              title: 'today deaths',
-                              value: snapshot.data!.todayDeaths.toString()),
-                          componentRecord(
-                              title: 'Recovered Cases',
-                              value: snapshot.data!.recovered.toString()),
-                          componentRecord(
-                              title: 'critical',
-                              value: snapshot.data!.critical.toString()),
-                        ],
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                } else {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            componentRecord(
+                              title: 'cases',
+                              value: snapshot.data!.cases.toString(),
+                            ),
+                            componentRecord(
+                                title: 'Active cases',
+                                value: snapshot.data!.active.toString()),
+                            componentRecord(
+                                title: 'death',
+                                value: snapshot.data!.deaths.toString()),
+                            componentRecord(
+                                title: 'today cases',
+                                value: snapshot.data!.todayCases.toString()),
+                            componentRecord(
+                                title: 'today deaths',
+                                value: snapshot.data!.todayDeaths.toString()),
+                            componentRecord(
+                                title: 'Recovered Cases',
+                                value: snapshot.data!.recovered.toString()),
+                            componentRecord(
+                                title: 'critical',
+                                value: snapshot.data!.critical.toString()),
+                          ],
+                        ),
                       ),
                     ),
+                  );
+                }
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CountryListScreen(),
                   ),
                 );
               },
-            ),
+              child: Container(
+                width: 200,
+                height: 40,
+                color: Colors.green,
+                child: Center(
+                  child: Text(
+                    'Countries Tracker',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
